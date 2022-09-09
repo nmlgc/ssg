314d68c295b592b6d49c16cc1440d5724a4c52a5
nmlgc <nmlgc@nmlgc.net>
2022-09-10 00:15:28 +0200

    [Platform] Add a safe buffer reading abstraction
    I could add a separate overload for n=1, but that would require the
    ugly std::reference_wrapper because std::optional<T&> is a complicated
    topic (see https://www.foonathan.net/2018/07/optional-reference/)… so
    let's just *not* do it and only ever return a std::span<T>, even for
    n=1. Looks prettier in the usage code, too!
    
    Part of M0002, funded by Arandui.

--- platform/c/buffer.h
+++ platform/c/buffer.h
@@ -6,10 +6,34 @@
 #pragma once
 
 #include <memory>
+#include <optional>
 #include <span>
 #include <vector>
 
 using BYTE_BUFFER_BORROWED = std::span<const uint8_t>;
+
+struct BYTE_BUFFER_CURSOR : private BYTE_BUFFER_BORROWED {
+	size_t cursor = 0;
+
+	// Reads up to [n] contiguous values of type T from the active cursor
+	// position if possible. If the function returns a valid span, all [n]
+	// objects are safe to access.
+	template <typename T> std::optional<std::span<const T>> next(size_t n = 1) {
+		auto cursor_new = (cursor + (sizeof(T) * n));
+		if((cursor_new > size()) || (cursor_new < cursor)) {
+			return std::nullopt;
+		}
+		auto ret = std::span<const T>(
+			reinterpret_cast<const T *>(data() + cursor), n
+		);
+		cursor = cursor_new;
+		return ret;
+	}
+
+	BYTE_BUFFER_CURSOR(BYTE_BUFFER_BORROWED buffer) :
+		BYTE_BUFFER_BORROWED(buffer) {
+	}
+};
 
 // Same semantics as the underlying unique_ptr: Can be either allocated or
 // empty.
@@ -32,6 +56,11 @@ public:
 
 	auto size() const {
 		return size_;
+	}
+
+	// Borrows a buffer with a cursor.
+	BYTE_BUFFER_CURSOR cursor() const {
+		return { { get(), size() } };
 	}
 };
 

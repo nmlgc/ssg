09dd8d8c5100cde9264cddd3eecd7c34f479a5bf
nmlgc <nmlgc@nmlgc.net>
2022-09-10 00:08:55 +0200

    [Platform] Don't wrap FileLoad()'s return value into std::optional
    BYTE_BUFFER_OWNED and every derived function has enough safety nets
    that this one turns out redundant… for now.
    
    Part of M0002, funded by Arandui.

--- platform/c/file.cpp
+++ platform/c/file.cpp
@@ -6,28 +6,28 @@
 #include "platform/file.h"
 #include <io.h>
 
-std::optional<BYTE_BUFFER_OWNED> FileLoad(const char *s, size_t size_limit)
+BYTE_BUFFER_OWNED FileLoad(const char *s, size_t size_limit)
 {
 	auto fp = fopen(s, "rb");
 	if(!fp) {
-		return std::nullopt;
+		return {};
 	}
 
 	auto size64 = _filelengthi64(fileno(fp));
 	if(size64 > size_limit) {
-		return std::nullopt;
+		return {};
 	}
 	const size_t size = size64;
 
 	auto buf = BYTE_BUFFER_OWNED{ size };
 	if(!buf) {
-		return std::nullopt;
+		return {};
 	}
 
 	auto elements_read = fread(buf.get(), size, 1, fp);
 	fclose(fp);
 	if(elements_read != 1) {
-		return std::nullopt;
+		return {};
 	}
 
 	return std::move(buf);
--- platform/file.h
+++ platform/file.h
@@ -5,11 +5,10 @@
 
 #pragma once
 
-#include <optional>
 #include "platform/buffer.h"
 
 // Loads the file with the given name into a newly allocated buffer, if
 // possible.
-std::optional<BYTE_BUFFER_OWNED> FileLoad(
+BYTE_BUFFER_OWNED FileLoad(
 	const char *s, size_t size_limit = (std::numeric_limits<size_t>::max)()
 );

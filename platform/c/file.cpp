c31f0f5df9efdc8ec6f283b575783aa6068a9511
nmlgc <nmlgc@nmlgc.net>
2022-12-29 22:49:08 +0100

    [Platform] [Files] Implement file loading for statically sized structures
    Part of P0226, funded by alp-bib and Arandui.

--- platform/c/file.cpp
+++ platform/c/file.cpp
@@ -6,6 +6,21 @@
 #include "platform/file.h"
 #include <io.h>
 
+static size_t LoadInplace(std::span<uint8_t> buf, FILE*&& fp)
+{
+	if(!fp) {
+		return 0;
+	}
+	auto bytes_read = fread(buf.data(), 1, buf.size_bytes(), fp);
+	fclose(fp);
+	return bytes_read;
+}
+
+size_t FileLoadInplace(std::span<uint8_t> buf, const char *s)
+{
+	return LoadInplace(buf, fopen(s, "rb"));
+}
+
 BYTE_BUFFER_OWNED FileLoad(const char *s, size_t size_limit)
 {
 	auto fp = fopen(s, "rb");
@@ -24,9 +39,7 @@ BYTE_BUFFER_OWNED FileLoad(const char *s, size_t size_limit)
 		return {};
 	}
 
-	auto elements_read = fread(buf.get(), size, 1, fp);
-	fclose(fp);
-	if(elements_read != 1) {
+	if(LoadInplace({ buf.get(), size }, std::move(fp)) != size) {
 		return {};
 	}
 

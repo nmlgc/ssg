93b5bf2b546e8a5bf9a7c2dccc5a56909da6e1d5
nmlgc <nmlgc@nmlgc.net>
2023-09-03 13:40:15 +0200

    [Platform] [Unicode] Rename the `*_CODEPOINT` types to `*_CODEUNIT`
    "Code point" is factually wrong.
    
    Part of P0256, funded by Ember2528.

--- platform/unicode.h
+++ platform/unicode.h
@@ -11,10 +11,9 @@
 	#include "platform/c/unicode.h"
 #endif
 
-using UNICODE_CODEPOINT = UNICODE_STRING::value_type;
+using UNICODE_CODEUNIT = UNICODE_STRING::value_type;
 
 using PATH_STRING = UNICODE_STRING;
 using PATH_LITERAL = UNICODE_LITERAL;
-using PATH_CODEPOINT = UNICODE_CODEPOINT;
+using PATH_CODEUNIT = UNICODE_CODEUNIT;
 #define _PATH _UNICODE
-

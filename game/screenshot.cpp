a9ca0819325435a4f3bf981e74a8fcd6489b68eb
nmlgc <nmlgc@nmlgc.net>
2023-12-28 22:27:20 +0100

    [Graphics] Screenshots: Construct filenames as UTF-8
    They're going to be stored in directories whose base paths we retrieve
    from SDL's UTF-8 functions.
    
    Completes P0270, funded by Ember2528.

--- game/screenshot.cpp
+++ game/screenshot.cpp
@@ -14,13 +14,13 @@ template <typename T> constexpr unsigned int DecimalDigitsBound() {
 
 using NUM_TYPE = unsigned int;
 static constexpr unsigned int DIGITS_MAX = DecimalDigitsBound<NUM_TYPE>();
-static constexpr PATH_STRING_VIEW EXT = _PATH(".BMP");
+static constexpr std::u8string_view EXT = u8".BMP";
 
 static NUM_TYPE Num = 0;
-static PATH_STRING Buf;
+static std::u8string Buf;
 static size_t PrefixLen;
 
-void ScreenshotSetPrefix(PATH_STRING_VIEW prefix)
+void ScreenshotSetPrefix(std::u8string_view prefix)
 {
 	Buf.resize((prefix.length() + DIGITS_MAX + EXT.size() + 1));
 	std::copy(prefix.begin(), prefix.end(), Buf.begin());
@@ -38,13 +38,7 @@ std::unique_ptr<FILE_STREAM_WRITE> ScreenshotNextStream()
 		std::array<char, (DIGITS_MAX + 1)> num_str;
 		const auto num_len = sprintf(num_str.data(), "%04u", Num++);
 		auto buf_p = (Buf.begin() + PrefixLen);
-
-		// Stretch [num_str] to UTF-16 if necessary.
-		// std::copy() doesn't handle differing types.
-		for(auto i = decltype(num_len){0}; i < num_len; i++) {
-			*(buf_p++) = num_str[i];
-		}
-
+		buf_p = std::copy_n(num_str.data(), num_len, buf_p);
 		buf_p = std::copy(EXT.begin(), EXT.end(), buf_p);
 		*(buf_p++) = '\0';
 		auto ret = FileStreamWrite(Buf.data(), true);
--- game/screenshot.h
+++ game/screenshot.h
@@ -8,7 +8,7 @@
 #include "platform/file.h"
 
 // Required to enable the screenshot feature as a whole.
-void ScreenshotSetPrefix(PATH_STRING_VIEW prefix);
+void ScreenshotSetPrefix(std::u8string_view prefix);
 
 // Increments the screenshot number to the next file that doesn't exist yet,
 // then opens a write stream for that file.

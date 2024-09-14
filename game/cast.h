94393d2d1dd268fec1480449f6b8cacc2386ecb5
nmlgc <nmlgc@nmlgc.net>
2024-09-14 22:58:30 +0200

    [Maintenance] Annotate signedness-preserving downcasts
    Yup, we're going even beyond the Guidelines Support Library.
    
    Part of P0288, funded by Ember2528.

---
+++ game/cast.h
@@ -0,0 +1,24 @@
+/*
+ *   Directional casts for integers
+ *
+ *   Inspired by the Guidelines Support Library:
+ *
+ *   	https://github.com/microsoft/GSL
+ */
+
+#pragma once
+
+#include <type_traits>
+
+namespace Cast {
+
+template <class T, class F> constexpr T down(F&& f) noexcept
+{
+	using To = std::remove_reference_t<T>;
+	using From = std::remove_reference_t<F>;
+	static_assert(sizeof(To) < sizeof(From));
+	static_assert(std::is_signed_v<To> == std::is_signed_v<From>);
+	return static_cast<T>(std::forward<F>(f));
+}
+
+} // namespace Cast

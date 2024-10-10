nmlgc/ssg

commit 20493c4656d1e03623a208983f51e6c9433872b4

[Window system] Messages: Additionally keep all text in a single `std::string`
Yup, *additionally*, and redundantly. Eventually, we'd implement
automatic line breaks by splitting this full string into individual
lines at rendering time, but line breaking is complicated and it's way
too early to do this now. Especially because the Stage 4 dialog
actually relies on the wrapping behavior implemented in MWinMsg(),
which retains the first line because it's expected to be the name of
the speaking character.

Part of P0251, funded by Ember2528.

GIAN07/WindowSys.cpp
@@ -37,15 +37,17 @@ typedef struct tagMSG_WINDOW{
 	uint8_t	FaceState;	// 顔の状態
 	uint8_t	FaceTime;	// 顔表示用カウンタ
 
-	const char	*Msg[MSG_HEIGHT];	// 表示するメッセージへのポインタ
+	std::string_view	Msg[MSG_HEIGHT];	// 表示するメッセージへのポインタ
 
-	//char		MsgBuf[MSG_HEIGHT][MESSAGE_MAX];	// メッセージ格納配列の実体
+	// Contains all text from [Msg], concatenated with '\n'.
+	std::string	Text;
 
 	void MsgBlank() {
 		Line = 0;
 		for(auto& msg : Msg) {
-			msg = nullptr;
+			msg = {};
 		}
+		Text.clear();
 	}
 } MSG_WINDOW;
 
@@ -313,13 +315,19 @@ void MWinDraw(void)
 			SetBkMode(hdc,TRANSPARENT);
 
 			for(i=0;i<MsgWindow.Line;i++){
-				if(MsgWindow.Msg[i]==NULL) continue;	// 一応安全対策
+				const auto& line = MsgWindow.Msg[i];
+
+				// 一応安全対策
+				if(line.empty()) {
+					continue;
+				}
+
 				TextX = (x + FACE_W);
 				TextY = y +  8 + i*MsgWindow.FontDy;
 				SetTextColor(hdc,RGB(128,128,128));		// 灰色で１どっとずらして描画
-				TextOut(hdc,TextX+1,TextY,MsgWindow.Msg[i],strlen(MsgWindow.Msg[i]));
+				TextOut(hdc, (TextX + 1), TextY, line.data(), line.length());
 				SetTextColor(hdc,RGB(255,255,255));		// 白で表示すべき位置に表示
-				TextOut(hdc,TextX+0,TextY,MsgWindow.Msg[i],strlen(MsgWindow.Msg[i]));
+				TextOut(hdc, (TextX + 0), TextY, line.data(), line.length());
 			}
 
 			SelectObject(hdc,oldfont);
@@ -385,8 +393,7 @@ void MWinDraw(void)
 	}
 }
 
-// メッセージ文字列を送る //
-void MWinMsg(const char *s)
+void MWinMsg(std::string_view s)
 {
 	int			Line,i;
 
@@ -396,12 +403,17 @@ void MWinMsg(const char *s)
 		// すでに表示最大行数を超えていた場合 //
 		for(i=1;i<MsgWindow.MaxLine-1;i++) MsgWindow.Msg[i] = MsgWindow.Msg[i+1];
 		MsgWindow.Msg[Line-1] = s;
-		return;
+	} else {
+		// ポインタセット＆行数更新 //
+		MsgWindow.Msg[Line] = s;
+		MsgWindow.Line = Line+1;
 	}
 
-	// ポインタセット＆行数更新 //
-	MsgWindow.Msg[Line] = s;
-	MsgWindow.Line = Line+1;
+	MsgWindow.Text.clear();
+	for(const auto& s : MsgWindow.Msg) {
+		MsgWindow.Text += s;
+		MsgWindow.Text += '\n';
+	}
 }
 
 // 顔をセットする //

GIAN07/WindowSys.h
@@ -29,7 +29,6 @@
 // 最大数に関する定数 //
 #define WINITEM_MAX		20			// 項目の最大数
 #define WINDOW_DEPTH	10			// ウィンドウの深さ
-#define MESSAGE_MAX		100			// 一行に表示できる最大メッセージ長
 #define MSG_HEIGHT		5			// メッセージウィンドウの高さ
 
 // コマンドウィンドウの状態 //
@@ -112,7 +111,7 @@ void MWinForceClose(void);		// メッセージウィンドウを強制クロー�
 void MWinMove(void);			// メッセージウィンドウを動作させる(後で上と統合する)
 void MWinDraw(void);			// メッセージウィンドウを描画する(上に同じ)
 
-void MWinMsg(const char *s);			// メッセージ文字列を送る
+void MWinMsg(std::string_view str);	// メッセージ文字列を送る
 void MWinFace(BYTE faceID);		// 顔をセットする
 void MWinCmd(BYTE cmd);			// コマンドを送る
 

commit d8217bd3cd8faf29b4fb80fc5ce9c1bcd41ec1e2

[Bug] [Window system] Messages: Fix use-after-free error after box clearing
Introduced in 20493c4, and consisting of two parts:

• The [Msg] loop reset in MsgBlank() accidentally took a copy of each
  line's string_view. Therefore, it turned into a no-op, and was even
  optimized out of release builds.
• MWinMsg() added the entire [Msg] array to the [Text] string, even
  though it should only contain the valid lines, up to the current
  [Line] pointer.

Part of P0287, funded by Ember2528.

GIAN07/WindowSys.cpp
@@ -54,7 +54,7 @@ typedef struct tagMSG_WINDOW{
 
 	void MsgBlank() {
 		Line = 0;
-		for(auto msg : Msg) {
+		for(auto& msg : Msg) {
 			msg = {};
 		}
 		Text.clear();
@@ -530,8 +530,8 @@ void MWinMsg(Narrow::string_view s)
 	}
 
 	MsgWindow.Text.clear();
-	for(const auto s : MsgWindow.Msg) {
-		MsgWindow.Text += s;
+	for(decltype(MsgWindow.Line) i = 0; i < MsgWindow.Line; i++) {
+		MsgWindow.Text += MsgWindow.Msg[Line];
 		MsgWindow.Text += '\n';
 	}
 }
1aac1ae5f8e37abde288014adfacabd9a26f72ef
nmlgc <nmlgc@nmlgc.net>
2024-10-20 00:27:27 +0200

    [Main] [SDL] Use the native line rendering API
    Part of P0295, funded by Ember2528 and [Anonymous].

--- MAIN/main_sdl.cpp
+++ MAIN/main_sdl.cpp
@@ -36,10 +36,31 @@ int main(int argc, char** args)
 	}
 	defer(SDL_Quit());
 
+	// Use the backend API's line drawing algorithm, which at least gives us
+	// pixel-perfect accuracy with pbg's original 16-bit code when using
+	// Direct3D and framebuffer scaling. It does make sense to set this hint
+	// unconditionally because the native OpenGL line drawing algorithm is also
+	// slightly more accurate to the original game than the SDL algorithm when
+	// drawing longer lines. But in any case, we're talking differences of less
+	// than 1% of pixels here; both OpenGL and SDL algorithms are at least 97%
+	// accurate. For a graphical comparison, see
+	//
+	// 	https::/rec98.nmlgc.net/blog/2024-10-22#lines-2024-10-22
+	//
+	// And if we ever want 100% accuracy for every backend API, we can always
+	// reimplement Direct3D exact algorithm:
+	//
+	// 	https://github.com/nmlgc/ssg/issues/74
+	SDL_SetHint(SDL_HINT_RENDER_LINE_METHOD, "2");
+
 	// OpenGL is either *the* best or close to the best choice everywhere when
 	// it comes to performance (at least as long as we don't batch draw calls):
 	//
 	// 	https://rec98.nmlgc.net/blog/2024-10-22#benchmark-2024-10-22
+	//
+	// Yes, this means that players have to manually pick any of the Direct3D
+	// APIs to get accurate line drawing, but great performance on everything
+	// out of the box is more important.
 	SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengl");
 
 	if(!XInit()) {

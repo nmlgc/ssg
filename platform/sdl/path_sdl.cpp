d9f2e40ee9d95ad22f755f8460fd8a0a0719c871
nmlgc <nmlgc@nmlgc.net>
2024-12-29 01:37:52 +0100

    [Path] [SDL] Use the pref path on Linux
    The base path will not be writable by unprivileged users when using any
    kind of package.
    
    Part of P0302, funded by Ember2528.

--- platform/sdl/path_sdl.cpp
+++ platform/sdl/path_sdl.cpp
@@ -7,6 +7,7 @@
 #include <SDL_filesystem.h>
 
 #include "platform/path.h"
+#include "constants.h"
 
 constexpr auto SDL_free_deleter = [](auto* p) { SDL_free(p); };
 static std::unique_ptr<char8_t[], decltype(SDL_free_deleter)> PathData = {
@@ -19,11 +20,15 @@ std::u8string_view PathForData(void)
 	if(PathDataView.data() != nullptr) {
 		return PathDataView;
 	}
-	auto* path_base = reinterpret_cast<char8_t *>(SDL_GetBasePath());
+	#ifdef WIN32
+		auto* path_base = SDL_GetBasePath();
+	#else
+		auto* path_base = SDL_GetPrefPath(GAME_ORG, GAME_APP);
+	#endif
 	if(!path_base) {
 		return {};
 	}
-	PathData.reset(path_base);
+	PathData.reset(std::bit_cast<char8_t *>(path_base));
 	PathDataView = PathData.get();
 	return PathDataView;
 }
--- strings/title.h
+++ strings/title.h
@@ -1,1 +1,3 @@
+#define GAME_ORG "rec98"
+#define GAME_APP "sh01"
 #define GAME_TITLE "秋霜玉"

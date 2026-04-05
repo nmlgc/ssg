a4d1c0795ee8a50f49262f708110a42ae333321f
nmlgc <nmlgc@nmlgc.net>
2026-04-05 21:24:28 +0200

    [Headless] Lasers: Turn into a class
    Part of P0339, funded by Ember2528.

---
+++ ssg/internal/Laser.hpp
@@ -0,0 +1,66 @@
+/*
+ *   Linear laser class
+ *
+ */
+
+#pragma once
+
+#include "LASER.H"
+
+class C_LASER {
+public:
+	// 標準レーザーコマンド構造体
+	LASER_CMD LaserCmd;
+
+private:
+	// レーザーの本数
+	uint16_t LaserNow;
+
+	// レーザー格納用構造体
+	std::array<LASER_DATA, LASER_MAX> Laser;
+
+	// レーザー順番維持用配列
+	std::array<uint16_t, LASER_MAX> LaserInd;
+
+	// レーザーの進行方向をセットする
+	uint8_t laser_dir(uint16_t i);
+
+	// 種類により分岐し座標を更新する
+	void Lmove(LASER_DATA *lp);
+
+	// レーザーの当たり判定
+	void laser_hitchk(LASER_DATA *lp);
+
+	// 反射レーザーの移動
+	void REFL_move(LASER_DATA *lp);
+
+	// ﾘﾌﾚｸﾀｰとの当たり判定(ﾋｯﾄ->非０)
+	int REFL_hit(const LASER_DATA *lp);
+
+public:
+	// レーザーをセットする(難易度変更"有り")
+	void Set(void);
+
+	// レーザーをセットする(難易度変更"無し")
+	void SetEX(void);
+
+	// レーザーを動かす
+	void Move(void);
+
+	// レーザー全てに消去エフェクトをかける
+	void Clear(void);
+
+	// レーザー順序用配列の初期化
+	void IndSet(void);
+
+	const LASER_DATA_CSPAN Data(void) const noexcept {
+		return Laser;
+	}
+
+	const std::span<const uint16_t> Inds(void) const {
+		return std::span(LaserInd).first(LaserNow);
+	}
+};
+
+
+extern C_LASER Laser;

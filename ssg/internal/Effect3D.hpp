d022590865d441d997c875ffa3969cd58665fe67
nmlgc <nmlgc@nmlgc.net>
2026-04-13 01:28:44 +0200

    [Headless] 3D effects: Turn into a class
    One class for all of them, since some effects use several pieces of
    data. And keep around their state until these pieces get reused for
    other effects…
    Including some critical transformations of `static` local variables
    to class members that would have probably cost me a few hours to debug
    if I hadn't found them right now…
    
    Part of P0339, funded by Ember2528.

---
+++ ssg/internal/Effect3D.hpp
@@ -0,0 +1,75 @@
+/*
+ *   3D effect classes
+ *
+ */
+
+#pragma once
+
+#include "EFFECT3D.H"
+
+class C_EFFECT3D {
+private:
+	uint16_t cube_d;
+	uint16_t cube_dx;
+	uint16_t cube_dy;
+
+	Cube3D Cube[CUBE_MAX];
+	Star2D Star[STAR_MAX];
+	Rock3D Rock[ROCK_MAX];
+	WFLine2D WFLine;
+	FakeECLString FakeECLStr[FAKE_ECLSTR_MAX];
+	Stg6Raster S6Ras[S6RASTER_MAX];
+	Stg6Star S6Star[S3STAR_MAX]; // 兼用モノなのだ
+
+public:
+	void Init3DCube(void);
+	void Move3DCube(void);
+
+	void InitFakeECL(void);
+	void MoveFakeECL(void);
+
+	// ステージ４の背景となる岩の集団
+	void InitStg4Rock(void);
+
+	void MoveStg4Rock(void);
+
+	// コマンド送信
+	void SendCmdStg4Rock(uint8_t Cmd, uint8_t Param);
+
+	// ６面ラスター初期化
+	void InitStg6Raster();
+
+	// ６面ラスター動作
+	void MoveStg6Raster();
+
+	// ３面高速星初期化
+	void InitStg3Star();
+
+	// ３面高速星動作
+	void MoveStg3Star();
+
+	CUBE3D_CSPAN DataCube(void) const {
+		return Cube;
+	}
+	STAR2D_CSPAN DataStar(void) const {
+		return Star;
+	}
+	ROCK3D_CSPAN DataRock(void) const {
+		return Rock;
+	}
+	const WFLine2D& DataWFLine(void) const {
+		return WFLine;
+	}
+	FAKE_ECLSTR_CSPAN DataFakeECL(void) const {
+		return FakeECLStr;
+	}
+	STG6RASTER_CSPAN DataS6Ras(void) const {
+		return S6Ras;
+	}
+	STG6STAR_CSPAN DataS6Star(void) const {
+		return S6Star;
+	}
+};
+
+
+extern C_EFFECT3D Effect3D;

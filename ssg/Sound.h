91b06339c67e67cb8687d4beee487b07751a452e
nmlgc <nmlgc@nmlgc.net>
2026-04-28 23:53:30 +0200

    [Sound] Move IDs to a new header at the game logic level
    We're going to have to do *something* about decoupling all these
    `Snd_SEPlay()` calls from the logic level. Technically, sounds aren't
    logic, and the academically pretty solution would be a logical callback
    for each and every place where the original game played a sound. But
    that extra layer of indirection is way too much overengineering,
    especially since we can count the number of truly *necessary* callbacks
    on one hand.
    Similarly, we could use this occasion to re-engineer the per-ID
    instance numbers in a more type-safe way that might even remove the
    current need for backends to dynamically allocate their array of per-ID
    instance structures.
    But all of this is complex enough as it is, and sound effects are
    pretty much the least important aspect in the grand scheme of going
    headless. Let's keep it simple.
    
    Part of P0336, funded by Ember2528.

---
+++ ssg/Sound.h
@@ -0,0 +1,27 @@
+/*
+ *   サウンド(効果音番号)
+ *
+ */
+
+#pragma once
+
+#define SOUND_ID_KEBARI    0x00
+#define SOUND_ID_TAME      0x01
+#define SOUND_ID_LASER     0x02
+#define SOUND_ID_LASER2    0x03
+#define SOUND_ID_BOMB      0x04
+#define SOUND_ID_SELECT    0x05
+#define SOUND_ID_HIT       0x06
+#define SOUND_ID_CANCEL    0x07
+#define SOUND_ID_WARNING   0x08
+#define SOUND_ID_SBLASER   0x09
+#define SOUND_ID_BUZZ      0x0a
+#define SOUND_ID_MISSILE   0x0b
+#define SOUND_ID_JOINT     0x0c
+#define SOUND_ID_DEAD      0x0d
+#define SOUND_ID_SBBOMB    0x0e
+#define SOUND_ID_BOSSBOMB  0x0f
+#define SOUND_ID_ENEMYSHOT 0x10
+#define SOUND_ID_HLASER    0x11
+#define SOUND_ID_TAMEFAST  0x12
+#define SOUND_ID_WARP      0x13

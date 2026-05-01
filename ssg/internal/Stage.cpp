46af05d6b058f9d2d125bdcbd76f268afa07b1a2
nmlgc <nmlgc@nmlgc.net>
2026-05-01 22:44:35 +0200

    [Headless] 3D effects: Adopt logic code from the scroll subsystem
    Note how emptying the bodies of the `ExCmd()` functions for Stage 4 and
    Stage 6 means that we can no longer safely disambiguate them in the
    renderer. Since MSVC's `/OPT:ICF` folds every empty function to the
    same address, `ScrollDraw()` can then no longer distinguish those
    functions from `ScrollCmdDummy()`. It certainly took me a while to
    realize why the Windows Release builds suddenly didn't render regular
    stage backgrounds anymore…
    
    Part of P0336, funded by Ember2528.

--- ssg/internal/Stage.cpp
+++ ssg/internal/Stage.cpp
@@ -4,6 +4,7 @@
  */
 
 #include "ssg/internal/Stage.hpp"
+#include "EFFECT3D.H"
 
 C_STAGE Stage;
 
@@ -14,6 +15,7 @@ bool C_STAGE::Set(BUFFER_OWNED&& data)
 	GameCount = 0;
 	SclInfo.MsgFlag = false;
 	SclInfo.ReturnFlag = false;
+	Effect3D.Set(EFFECT3D_TYPE::NONE);
 
 	return (SCL_Head.get() != nullptr);
 }

51150c3ad2e4a7b799568ff3f525066b93760b48
nmlgc <nmlgc@nmlgc.net>
2026-05-10 02:33:24 +0200

    [Headless] SSG: Adopt updates during Game Over
    SEffect, the subsystem showing the Game Over animation, just *had* to
    be part of game logic due to `SEFC_STR1_2` using the RNG. Thus, it's
    completely possible for such an effect to exist – and to continue being
    animated – during the 120 frames of the `SEFC_GAMEOVER` animation,
    which advances the RNG state after the continue if the player decides
    to use one.
    And sure, you can't save continue usage in the old replay format, but
    full-game replays across multiple continues will be a feature of the
    new format.
    
    Part of P0340, funded by Ember2528.

--- ssg/internal/SSG.cpp
+++ ssg/internal/SSG.cpp
@@ -17,6 +17,7 @@ constexpr HOOKS HOOKS_EMPTY = {
 	.Snd_SEStop = [](uint8_t, HOOKS *) {},
 	.Boss_HPSumAtStart = [](const uint32_t, HOOKS *) {},
 	.Boss_Defeat = [](const BOSS_DATA *, HOOKS *) {},
+	.GameOver = [](HOOKS *) {},
 };
 
 HOOKS Hooks;
@@ -97,6 +98,8 @@ void C_SSG::StageInit(void)
 
 void C_SSG::Move(INPUT_BITS input)
 {
+	assert(Viv.GameOverTimer == 0);
+
 	Stage.Move(input);
 	Effect3D.Move();
 
@@ -116,4 +119,16 @@ void C_SSG::Move(INPUT_BITS input)
 	// この２行の位置を変更しました //
 	Viv.Move(input, Stage.InMsg());
 	MaidTama.Move();
+}
+
+unsigned int C_SSG::MoveGameOver(void)
+{
+	if(Viv.GameOverTimer <= 0) {
+		return 0;
+	}
+
+	Viv.GameOverTimer--;
+	Fragment.Move();
+	SEffect.Move();
+	return Viv.GameOverTimer;
 }

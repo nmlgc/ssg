ec6545262321e624060cc6d89d8bc49783e447a2
nmlgc <nmlgc@nmlgc.net>
2026-05-17 05:44:05 +0200

    [Build] Hatoyama: Adopt build script
    Part of P0336, funded by [Anonymous].

--- hatoyama/Tupfile.linux.lua
+++ hatoyama/Tupfile.linux.lua
@@ -1,3 +1,38 @@
 tup.import("TOOLCHAIN=gcc")
 tup.include("libs/tupblocks/toolchain." .. TOOLCHAIN .. ".lua")
 tup.include("libs/BLAKE3.lua")
+
+HATOYAMA_LINK.cflags += { "-DLINUX" }
+
+function BuildHatoyama()
+	local PLATFORM_LINK = EnvConfig("sdl3", "pangocairo", "fontconfig")
+	local LAYERS_LINK = EnvConfig("libwebp", "ogg", "vorbis", "vorbisfile")
+	local BLAKE3_LINK = (EnvConfig("libblake3") or BuildBLAKE3(CONFIG, 0))
+
+	-- Since Pango/Cairo adds -pthread to a later configuration, the C++
+	-- standard library must also be compiled with this flag.
+	local base_cfg = CONFIG:branch({ cflags = "-pthread" })
+
+	local game_cfg = CONFIG:branch(
+		BLAKE3_LINK, LAYERS_LINK, base_cfg:cxx_std_modules(), HATOYAMA_LINK
+	)
+	local platform_cfg = game_cfg:branch(PLATFORM_LINK)
+
+	local game_src = (HATOYAMA.glob("platform/c/*.cpp"))
+	game_src += HATOYAMA_SRC
+
+	local platform_src = HATOYAMA.glob("platform/sdl/*.cpp")
+	platform_src += HATOYAMA.glob("platform/miniaudio/*.cpp")
+	platform_src += HATOYAMA.glob("platform/pangocairo/*.cpp")
+
+	-- Clang does not like C being compiled with clang++, and non-C++ clang
+	-- does not like module-related switches.
+	local c_src = HATOYAMA.glob("platform/miniaudio/*.c")
+
+	local obj = (
+		game_cfg:branch(HATOYAMA_COMPILE):cxx(game_src) +
+		platform_cfg:branch(HATOYAMA_COMPILE):cxx(platform_src) +
+		base_cfg:branch(HATOYAMA_COMPILE, HATOYAMA_LINK):cc(c_src)
+	)
+	return platform_cfg:branch({ linputs = obj })
+end

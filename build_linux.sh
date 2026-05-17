7eef4bf3bd5d5b0a2cba7a39db9bf008a1a9888b
nmlgc <nmlgc@nmlgc.net>
2026-05-17 23:37:18 +0200

    [Build] Allow running the top-level build scripts from any repo path
    Rather hilarious how the combination of `%~dp0` and the backslash
    translation feature of Git for Windows's `sh` make this much easier on
    Batch than on POSIX-compliant shell where we have to pass the script
    directory as a parameter…
    
    Part of P0336, funded by Ember2528.

--- build_linux.sh
+++ build_linux.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 
-./version_from_git.sh
+root="$(dirname "$0")"
 
 # We can't commit this file directly because Tup's database initialization
 # check literally just looks for the directory, and refuses to auto-initialize
 # the database if it already exists. (Also, Windows doesn't need it.)
-[ ! -d .tup ] && tup init
-[ ! -f .tup/options ] &&
+[ ! -d "$root/.tup" ] && tup init
+[ ! -f "$root/.tup/options" ] &&
 echo "[updater]
-	full_deps = 1 ; Necessary to track compiler updates on Linux" > .tup/options
+	full_deps = 1 ; Necessary to track compiler updates on Linux
+" > "$root/.tup/options"
+
+"$root/version_from_git.sh"
 
 # Libraries that either aren't packaged by any distro or that we always want
 # to use the vendored version of
-./submodules_check.sh \
-	libs/tupblocks \
-	libs/dr_libs \
-	libs/miniaudio \
+"$root/submodules_check.sh" \
+	$root/libs/tupblocks \
+	$root/libs/dr_libs \
+	$root/libs/miniaudio \
 	|| exit 1
 
-. ./libs/tupblocks/tupblocks.sh
+. "$root/libs/tupblocks/tupblocks.sh"
 
 # Libraries that are supposed to be installed through the system's package
 # manager
@@ -34,7 +37,7 @@ pkg_config_env_required \
 # Vendored libraries that we only use when they aren't installed system-wide
 pkg_config_env_optional libblake3
 ! pkg-config --exists libblake3 && {
-	./submodules_check.sh libs/BLAKE3 || exit 1;
+	./submodules_check.sh "$root/libs/BLAKE3" || exit 1;
 }
 
 toolchain_detect_via_cc

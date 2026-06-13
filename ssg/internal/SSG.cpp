200f25f28f3ebe91b322666059a804073b412d24
nmlgc <nmlgc@nmlgc.net>
2026-06-13 23:00:07 +0200

    [Headless] Maid: Add miss and bomb hooks
    Obviously a great omission if the upcoming CLI didn't report them.
    
    Part of P0341, funded by Ember2528 and [Anonymous].

--- ssg/internal/SSG.cpp
+++ ssg/internal/SSG.cpp
@@ -14,6 +14,8 @@ constexpr HOOKS HOOKS_EMPTY = {
 	.Snd_SEStop = [](uint8_t, HOOKS *) {},
 	.Boss_HPSumAtStart = [](const uint32_t, HOOKS *) {},
 	.Boss_Defeat = [](const BOSS_DATA *, HOOKS *) {},
+	.Viv_Dead = [](HOOKS *) {},
+	.Viv_Bomb = [](HOOKS *) {},
 	.GameOver = [](HOOKS *) {},
 };
 
